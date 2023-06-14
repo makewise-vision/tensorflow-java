b6ae4f338b4b1c76c2e47b8f41047eb0cb13bc93
Ivo Lucas <ivolucas@gmail.com>
2023-06-14 21:14:20 +0100

    Remove avx512 error

--- tensorflow-core/tensorflow-core-api/build.sh
+++ tensorflow-core/tensorflow-core-api/build.sh
@@ -17,11 +17,9 @@ if [[ -d $BAZEL_VC ]]; then
     elif [[ "${EXTENSION:-}" == *-avx2* ]]; then
         export BUILD_FLAGS="--copt=//arch:AVX --copt=//arch:AVX2 --define=override_eigen_strong_inline=true"
     elif [[ "${EXTENSION:-}" == *-avx512f* ]]; then
-        export BUILD_FLAGS="--copt=//arch:AVX --copt=//arch:AVX2 --copt=//arch:AVX512 --copt=//arch:AVX512F --define=override_eigen_strong_inline=true"
+        export BUILD_FLAGS="--copt=//arch:AVX --copt=//arch:AVX2 --copt=//arch:AVX512F --define=override_eigen_strong_inline=true"
     elif [[ "${EXTENSION:-}" == *-avx512vnni* ]]; then
-        export BUILD_FLAGS="--copt=//arch:AVX --copt=//arch:AVX2 --copt=//arch:AVX512 --copt=//arch:AVX512F --copt=//arch:AVX512VNNI --define=override_eigen_strong_inline=true"
-    elif [[ "${EXTENSION:-}" == *-avx512* ]]; then
-        export BUILD_FLAGS="--copt=//arch:AVX --copt=//arch:AVX2 --copt=//arch:AVX512 --define=override_eigen_strong_inline=true"
+        export BUILD_FLAGS="--copt=//arch:AVX --copt=//arch:AVX2 --copt=//arch:AVX512F --copt=//arch:AVX512VNNI --define=override_eigen_strong_inline=true"
     else
         export BUILD_FLAGS="--copt=//arch:AVX --define=override_eigen_strong_inline=true"
     fi
@@ -36,11 +34,9 @@ else
     elif [[ "${EXTENSION:-}" == *-avx2* ]]; then
         export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mfma --copt=-mavx --copt=-mavx2 --linkopt=-lstdc++ --host_linkopt=-lstdc++"
     elif [[ "${EXTENSION:-}" == *-avx512f* ]]; then
-        export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mfma --copt=-mavx --copt=-mavx2 --copt=-mavx512 --copt=-mavx512f --linkopt=-lstdc++ --host_linkopt=-lstdc++"
+        export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mfma --copt=-mavx --copt=-mavx2 --copt=-mavx512f --linkopt=-lstdc++ --host_linkopt=-lstdc++"
     elif [[ "${EXTENSION:-}" == *-avx512vnni* ]]; then
-        export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mfma --copt=-mavx --copt=-mavx2 --copt=-mavx512 --copt=-mavx512f --copt=-mavx512vnni --linkopt=-lstdc++ --host_linkopt=-lstdc++"
-    elif [[ "${EXTENSION:-}" == *-avx512* ]]; then
-        export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mfma --copt=-mavx --copt=-mavx2 --copt=-mavx512 --linkopt=-lstdc++ --host_linkopt=-lstdc++"
+        export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mfma --copt=-mavx --copt=-mavx2 --copt=-mavx512f --copt=-mavx512vnni --linkopt=-lstdc++ --host_linkopt=-lstdc++"
     else
         export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --linkopt=-lstdc++ --host_linkopt=-lstdc++"
     fi
